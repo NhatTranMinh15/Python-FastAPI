aa02a2b242e48ed3c70b4f37a141606026889cf2
NhatTranMinh15 <Nhat.TranMinh@nashtechglobal.com>
2024-08-26 14:54:43 +0700

    Add Custom Message

--- app/services/exceptions.py
+++ app/services/exceptions.py
@@ -7,6 +7,9 @@ class BadRequestException(HTTPException):
     def __init__(self) -> None:
         super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail="Bad request")
 
+    def __init__(self, detail) -> None:
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
+
 
 class ConflictException(HTTPException):
     def __init__(self) -> None:
