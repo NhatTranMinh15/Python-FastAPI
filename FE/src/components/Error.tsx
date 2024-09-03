78e78be70f57aa64991cb07c702c4b1d3fd9d95f
NhatTranMinh15 <Nhat.TranMinh@nashtechglobal.com>
2024-09-03 09:52:44 +0700

    Remove FE folder

--- FE/src/components/Error.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "react-bootstrap";
-
-export const Error = () => {
-    return ( 
-        <Container>
-            <h1>Error</h1>
-        </Container>
-    );
-}
--- FE/src/models/PageModel.tsx
+++
@@ -1,7 +0,0 @@
-export interface PageResponseModel<T> {
-    items: T[];
-    total: number;
-    page: number;
-    size: number;
-    pages: number;
-}
